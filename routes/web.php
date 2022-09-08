e9089b70aea5dd8bbd0627a9c62a9f5cbd533793
Daniel B <76835848+Danielbp019@users.noreply.github.com>
2022-09-08 10:06:59 -0500

    v2
    rutas, cambio de una vista de vue a blade por motivos de estudio y añadir index.blade

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,19 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+}); */
 
-Route::middleware([
+Route::view('/', 'index');
+/* Es básicamente lo mismo de la ruta de arriba solo que se desvía de una vista en vue a una en blade (se puede dejar con la ruta original y nada cambia) y todos los enlaces y informaciones para la version de laravel o la de php están en la vista index.*/
+
+/* Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
@@ -33,3 +36,11 @@ Route::middleware([
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
+ */
+
+ Route::get('dashboard', [App\Http\Controllers\PageController::class, 'dashboard'])
+ ->middleware('auth:sanctum')
+ ->name('dashboard');
+
+ Route::resource('notes', App\Http\Controllers\NoteController::class)
+ ->middleware('auth:sanctum');
