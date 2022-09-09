3b674d2f06a02802aee47e77aed70966e95774c3
Daniel B <76835848+Danielbp019@users.noreply.github.com>
2022-09-08 21:22:53 -0500

    v5
    cambio del nombre del archivo index a Index en la carpeta PAGES/NOTES. Se añadió código adicional para mostrar datos y una tabla en index

--- resources/js/Pages/Notes/index.vue
+++ resources/js/Pages/Notes/index.vue
@@ -1,5 +1,8 @@
 <script setup>
     import AppLayout from '@/Layouts/AppLayout.vue';
+    defineProps({
+    notes: Array,
+    })
     </script>
     
     <template>
@@ -18,6 +21,27 @@
                                 <h3 class="text-lg text-gray-900">Listado de notas</h3> <!-- texto grande en color gris de 900 intensidad -->
                                 <p class="text-sm text-gray-600">Toma un registro y ejecuta una función</p> <!-- texto mediano color gris intensidad 600 -->
 
+                            </div>
+                        </div>
+                        <div class="md:col-span-2 mt-5 md:mt-0">
+                            <div class="shadow bg-white md:rounded-md p-4">
+                                <table>
+                                    <tr v-for="note in notes"> <!-- reemplaza el foreach de blade -->
+                                        <td class="border px-4 py-2">
+                                            {{note.excerpt}}
+                                        </td>
+                                        <td class="px-4 py-2">
+                                            <inertia-link :href="route ('notes.show', note.id)">
+                                                Ver
+                                            </inertia-link>
+                                        </td>
+                                        <td class="px-4 py-2">
+                                            <inertia-link :href="route ('notes.show', note.id)">
+                                                Editar
+                                            </inertia-link>
+                                        </td>
+                                    </tr>
+                                </table>
                             </div>
                         </div>
                     </div>
